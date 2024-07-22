f1049ee789da3028ce22dad5f2b6080ddc028a7c
Virutilla1986 <jacksonmartinez1999jm@gmail.com>
2024-07-22 20:01:20 +0200

    Update info-cuentas-oficiales.js

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -1,26 +1,17 @@
 let media = 'https://qu.ax/NqZN.mp4'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let str = `💙 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
-💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-💖 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐈
-${bot}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *GITHUB*
-*${md}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *INSTAGRAM - ASISTENCIA*
-*${ig}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *YOUTUBE*
-*${yt}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *FACEBOOK*
-*${fb}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
-*If you have doubts, suggestions or questions just write on Instagram.*`
+let str = `𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼, 𝗮𝗾𝘂𝗶 𝘁𝗲 𝗲𝘅𝗽𝗹𝗶𝗰𝗮𝗺𝗼𝘀 𝗹𝗼 𝗾𝘂𝗲 𝗻𝗲𝗰𝗲𝘀𝗶𝘁𝗮𝘀 𝗽𝗮𝗿𝗮 𝗼𝗯𝘁𝗲𝗻𝗲𝗿 𝘁𝘂 𝗯𝗼𝘁 𝗲𝗻 𝘁𝘂 𝗴𝗿𝘂𝗽𝗼....
+
+1) Hay dos formas de tener un bot en tu grupo, termux y por servidor..
+
+Por servidor, necesitas tener un WhatsApp para el bot, te recomiendo que solo sea para el bot..
+
+una vez que tengas puedes contactar con Punisher y pedirle que te ponga un bot en el servidor 24/7
+
+
+
+*Si tienen dudas, preguntas solo escriban por al número....*\n`
 await conn.sendButton(m.chat, str, wm, media, [
 ['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
 ['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 💗', '#owner'],
