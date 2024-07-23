84534acdee2b20c003d6dcb50a5bc7e1c3d6f1e3
Virutilla1986 <jacksonmartinez1999jm@gmail.com>
2024-07-23 14:37:12 +0200

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -572,7 +572,7 @@ ${margen}
 ✓ _${usedPrefix}unete *enlace*_
 ✓ _${usedPrefix}dardiamantes *cantidad*_
 ✓ _${usedPrefix}darxp *cantidad*_
-✓ _${usedPrefix}dargatacoins *cantidad*_
+✓ _${usedPrefix}darmundoBotcoins *cantidad*_
 ✓ _${usedPrefix}addprem | userpremium *@tag* *cantidad*_
 ✓ _${usedPrefix}addprem2 | userpremium2 *@tag* *cantidad*_
 ✓ _${usedPrefix}addprem3 | userpremium3 *@tag* *cantidad*_
@@ -586,7 +586,7 @@ ${margen}
 ✓ _${usedPrefix}comunicargrupos *texto*_
 ✓ _${usedPrefix}borrartmp | cleartmp_
 ✓ _${usedPrefix}delexp *@tag*_
-✓ _${usedPrefix}delgatacoins *@tag*_
+✓ _${usedPrefix}delmundobotcoins *@tag*_
 ✓ _${usedPrefix}deldiamantes *@tag*_
 ✓ _${usedPrefix}reiniciar | restart_
 ✓ _${usedPrefix}ctualizar | update_
@@ -595,7 +595,7 @@ ${margen}
 ✓ _${usedPrefix}listapremium | listprem_
 ✓ _${usedPrefix}añadirdiamantes *@tag cantidad*_
 ✓ _${usedPrefix}añadirxp *@tag cantidad*_
-✓ _${usedPrefix}añadirgatacoins *@tag cantidad*_
+✓ _${usedPrefix}añadirmundobotscoins *@tag cantidad*_
 `.trim()
 await conn.sendFile(m.chat, gataImg, 'lp.jpg', menu, m, fakeChannel, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: redesMenu }}})
 //conn.sendFile(m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu, fkontak)
